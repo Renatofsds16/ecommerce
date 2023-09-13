2376ad36aae6723cd14851c2389538894d331d35
Renato Firmino <renatofss16@gmail.com>
2023-09-13 02:44:22 -0300

    models finalizado

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ValidationError
+import re
+from utils.validacpf import valida_cpf
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    bith_date = models.DateField()
+    cpf = models.CharField(max_length=15)
+    road = models.CharField(max_length=50)
+    number = models.CharField(max_length=5)
+    complement = models.CharField(max_length=30)
+    neighborhood = models.CharField(max_length=30) # bairro
+    cep = models.CharField(max_length=8)
+    city = models.CharField(max_length=30)
+    state = models.CharField(
+        default='AC',
+        max_length=2,
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+    )
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    def clean(self):
+        error_messeger = {}
+        if not valida_cpf(self.cpf):
+            error_messeger['cpf'] = 'cpf invalido'
+
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            error_messeger['cep'] = 'cep invalido'
+
+        if error_messeger:
+            raise ValidationError(error_messeger)
+        
+    class Meta:
+        verbose_name = 'perfil'
+        verbose_name_plural = 'perfis'
